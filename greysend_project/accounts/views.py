c0ce14b5328f2f2ff45293341b54a00c23176969
matteo <hu.igor@yahoo.com>
2023-05-19 12:04:23 -0400

    Auth and Non-auth navbars implemented. Users can now sign up, log in, and log out. Note: sign up redirect page still not implemeted, but log in/log out redirect pages are.

--- greysend_project/accounts/views.py
+++ greysend_project/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import AccountUser
 from django.contrib.auth.models import User
-from django.contrib.auth import login
-from forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
@@ -23,6 +24,28 @@ def signup(request):
             user = User.objects.create_user(request.POST['username'],
                                             password=request.POST['password1'])
             user.save()
-            login(request, user)
+            # login(request, user)
+        #retirm login page
         return render(request, 'signup.html', {"form":UserCreationForm})  
 # Compare this snippet from greysend_project/accounts/models.py:
+
+
+def log_in(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {'form': AuthenticationForm})
+    elif request.method=='POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            #to implement "profile.html"
+            return redirect('list_users')  
+        else:
+            #todo: implement error page
+            return redirect("error_no_user.html")
+        
+def log_out(request):
+    #get current user
+    logout(request)
+    return redirect('index')
